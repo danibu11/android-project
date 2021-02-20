8f27c43e7b56fd6fabb59d070ce9ac5882ba0a96
ShacharAngel <shachar.engel@gmail.com>
2021-02-20 16:55:42 +0200

    added DBHelper with 2 tables for users and tasks, added credentials validation to login screen, added saveToDB for the user class

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "MainActivity"; //for logging
+
     TextView title;
     ListView lv;
 
@@ -22,6 +25,12 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    //as straightforward as can be
+    private void deleteDB(){
+        Log.d(TAG, "delete DB");
+        this.deleteDatabase("buchnitzDB");
+    }
 }
 /*
 check your current branch name and status at any time (git status)
@@ -35,3 +44,24 @@ check your current branch name and status at any time (git status)
 -push your branch to git (git push/git push upstream bla bla if its the first time [git will tell you what to write])
 -go to git website and open a merge request from your branch to the target branch (99.99% of the time your target branch is develop)
 */
+
+
+
+
+
+/*
+/// USE THIS WHEN USER FINISHED FILLING THE SIGN UP FORM, DONT FORGET TO ADD FIELDS IN DBHELPER IF YOU NEED MORE COLUMNS IN THE TABLE ///
+// this is an example of creating  user and adding it to the database
+ try {
+    User testUser = new User(1, "shachar", "angel", "hebrew", 33, "gaza strip", "testPass", "aa@aa.com"); // REMEMBER USER ID IS UNIQUE!!!
+    testUser.saveToDB(this);
+} catch (Exception ex) {
+    Log.d("Login Activity", "threw an exception!", ex);
+}
+
+/// USED IN LOGIN ACTIVITY ///
+// this is an example of how to get all the users from the db and going over the returned users list
+ArrayList<User> allEntriesUpdated = DBHelper.getAllUsersFromDB(this);
+for (int i = 0; i < allEntries.size(); i++) {
+    Log.d("DEBUG_USERS", allEntries.get(i).toString());
+}*/
